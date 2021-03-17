9b975f1581500892df1de1b5212762024b5a73e9
Daljit Singh <daljit.dokal@yahoo.co.nz>
2021-03-17 15:50:33 +1300

    Typo fixed.

--- 1-install-utilities.sh
+++ 1-install-utilities.sh
@@ -14,10 +14,8 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc |
     gpg --dearmor |
     sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
 
-
 # Update repository information and install the azure-cli
-sudo apt-get update
-sudo apt-get install azure-cli
+sudo apt-get update && apt-get install azure-cli
 
 # Add the Azure CLI software repository (skip this step on ARM64 Linux distributions):
 AZ_REPO=$(lsb_release -cs)
@@ -28,4 +26,4 @@ echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO 
 # Install GitHub CLI
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 sudo apt-add-repository https://cli.github.com/packages
-sudo+ apt-get update && sudo apt-get install gh
+sudo apt-get update && sudo apt-get install gh
